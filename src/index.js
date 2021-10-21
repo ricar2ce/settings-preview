571d16c65e2fb0876550e8110aa61e45e9dd8434
riKr2ce <ricar2ce@gmail.com>
2021-10-21 16:27:23 -0400

    CHG: Layouts for the form

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Row, Col, Space } from 'antd';
 import 'antd/dist/antd.css';
+
+import "@fontsource/fira-sans";
+import './index.css'
+
+import Preview from "./components/Preview/Preview";
+import SettingsForm from "./components/SettingsForm/SettingsForm";
 
 ReactDOM.render(
   <React.StrictMode>
-      <></>
+      <Row justify="space-between" align="middle">
+          <Col  xs={24} sm={24} md={12} lg={12} xl={12}>
+              <SettingsForm/>
+          </Col>
+          <Col xs={0} sm={0} md={12} lg={12} xl={12}>
+              <Space align="center">
+                  <Preview url='http://direccion.dominio.com' color='blue' />
+              </Space>
+          </Col>
+      </Row>
   </React.StrictMode>,
   document.getElementById('root')
 );
